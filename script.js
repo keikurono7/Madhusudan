a5f92f1d35f6f342a2d183ead448ea427405999b
Madhusudan <125114994+keikurono7@users.noreply.github.com>
2024-02-17 13:11:29 +0530

    added dino file and a few optimizations

--- script.js
+++ script.js
@@ -32,18 +32,37 @@ var now =  new Date();
 var datetime = now.toLocaleString();
 document.getElementById("datetime").innerHTML = datetime;
 
-function translateFunction(){
-    document.getElementById("up").style.transform = 'translateY(-200%)';
+function translateFunction1(){
+    document.getElementById("up").style.transform = 'translateY(-800%)';
     document.getElementById("up").style.transition=" all 2s";
-    document.getElementById("down").style.transform = 'translateY(200%)';
+    document.getElementById("nav").style.transform = 'translateY(-600%)';
+    document.getElementById("nav").style.transition=" all 2s";
+    document.getElementById("down").style.transform = 'translateY(400%)';
     document.getElementById("down").style.transition=" all 2s";
     setTimeout(()=>{
         document.getElementById("era").style.display = 'none';
         document.getElementById("menup").style.display = 'flex';
-    },700);
+    },500);
     setTimeout(()=>{
         document.getElementById("menup").style.transform = 'translate(0%,30%)';
         document.getElementById("menup").style.transition=" all 2s";
-    },1200);
+    },600);
     
+}
+
+function translateFunction2(){
+    document.getElementById("menup").style.transform = 'translate(-400%,30%)';
+    document.getElementById("menup").style.transition=" all 3s";
+    setTimeout(()=>{
+        document.getElementById("era").style.display = 'flex';
+        document.getElementById("menup").style.display = 'none';
+    },500);
+    setTimeout(()=>{
+        document.getElementById("up").style.transform = 'translateY(0)';
+        document.getElementById("up").style.transition=" all 2s";
+        document.getElementById("nav").style.transform = 'translateY(0)';
+        document.getElementById("nav").style.transition=" all 2s";
+        document.getElementById("down").style.transform = 'translateY(0)';
+        document.getElementById("down").style.transition=" all 2s";
+    },600);
 }
